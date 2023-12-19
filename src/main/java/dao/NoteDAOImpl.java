c8680ebe44aafefa86723ddd26ddffa97ed11442
Lu <mandy951753@gmail.com>
2023-12-19 15:26:24 +0800

    note

---
+++ src/main/java/dao/NoteDAOImpl.java
@@ -0,0 +1,41 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteDAOImpl implements NoteDAO {
+
+    private List<String> notes = new ArrayList<>();
+
+    @Override
+    public List<String> getAllNotes() {
+        return notes;
+    }
+
+    @Override
+    public String getNoteById(int id) {
+        if (id >= 0 && id < notes.size()) {
+            return notes.get(id);
+        }
+        return null;
+    }
+
+    @Override
+    public void addNote(String note) {
+        notes.add(note);
+    }
+
+    @Override
+    public void updateNoteById(int id, String updatedNote) {
+        if (id >= 0 && id < notes.size()) {
+            notes.set(id, updatedNote);
+        }
+    }
+
+    @Override
+    public void deleteNoteById(int id) {
+        if (id >= 0 && id < notes.size()) {
+            notes.remove(id);
+        }
+    }
+}
